83b1fa5a3efbfc7b34fee7144d023fddd161db2b
Vladislav Belousov <vladbelousov@yahoo.com>
2024-08-21 08:31:42 +0100

    Load geo sets

--- engine/model/m_2_model_view.cpp
+++ engine/model/m_2_model_view.cpp
@@ -21,16 +21,29 @@
 void
 loki::M2ModelView::on_fully_loaded()
 {
-  data = reinterpret_cast<Data*>(buffer.data());
-  ASSERT(data->id[0] == 'S' && data->id[1] == 'K' && data->id[2] == 'I' && data->id[3] == 'N');
+  header = reinterpret_cast<Header*>(buffer.data());
+  ASSERT(header->id[0] == 'S' && header->id[1] == 'K' && header->id[2] == 'I' && header->id[3] == 'N');
 
-  const u16* index_lookup = reinterpret_cast<u16*>(&buffer[data->index.offset]);
-  const u16* triangles = reinterpret_cast<u16*>(&buffer[data->tris.offset]);
+  const u16* index_lookup = reinterpret_cast<u16*>(&buffer[header->index.offset]);
+  const u16* triangles = reinterpret_cast<u16*>(&buffer[header->tris.offset]);
 
-  raw_indices.resize(data->tris.number);
-  for (u32 i = 0; i < data->tris.number; ++i) {
+  raw_indices.resize(header->tris.number);
+  for (u32 i = 0; i < header->tris.number; ++i) {
     raw_indices[i] = index_lookup[triangles[i]];
   }
 
   spdlog::info("Loaded indices: {}", raw_indices.size());
+
+  // Render ops
+  const M2ModelGeoset* ops = reinterpret_cast<M2ModelGeoset*>(buffer.data() + header->sub.offset);
+
+  u32 istart = 0;
+  for (u32 i = 0; i < header->sub.number; ++i) {
+    auto& hd_geo = raw_geosets.emplace_back(ops[i]);
+    hd_geo.istart = istart;
+    istart += hd_geo.icount;
+    hd_geo.display = hd_geo.id == 0;
+  }
+
+  spdlog::info("Loaded geo sets: {}", raw_geosets.size());
 }
--- engine/model/m_2_model_view.h
+++ engine/model/m_2_model_view.h
@@ -19,11 +19,104 @@
 
 #include "engine/asset/asset.h"
 #include "engine/utils/types.h"
+#include "glm/vec3.hpp"
 
 namespace loki {
 
+#pragma pack(push, 1)
+
+  struct M2ModelGeoset
+  {
+    u32 id;                      // mesh part id?
+    u16 vstart;                  // first vertex, Starting vertex number.
+    u16 vcount;                  // num vertices, Number of vertices.
+    u16 istart;                  // first index, Starting triangle index (that's 3* the number of triangles drawn so far).
+    u16 icount;                  // num indices, Number of triangle indices.
+    u16 number_of_skinned_bones; // number of bone indices, Number of elements in the bone lookup table.
+    u16 start_bones;             // ? always 1 to 4, Starting index in the bone lookup table.
+    u16 root_bone;               // root bone?
+    u16 number_of_bones;         //
+    glm::vec3 bounding_box[2];
+    float radius;
+  };
+
+  // Same as M2ModelGeoset but with an u32 as istart, to handle index > 65535 (present in HD models)
+  struct M2ModelGeosetHD
+  {
+    M2ModelGeosetHD()
+      : id(-1)
+      , vstart(0)
+      , vcount(0)
+      , istart(0)
+      , icount(0)
+      , number_of_skinned_bones(0)
+      , start_bones(0)
+      , root_bone(0)
+      , number_of_bones(0)
+      , radius(0)
+      , display(false)
+      , bounding_box()
+    {
+      bounding_box[0] = glm::vec3(0, 0, 0);
+      bounding_box[1] = glm::vec3(0, 0, 0);
+    }
+
+    explicit M2ModelGeosetHD(const M2ModelGeoset& geo)
+      : id(geo.id & 0x7FFF)
+      , vstart(geo.vstart)
+      , vcount(geo.vcount)
+      , istart(geo.istart)
+      , icount(geo.icount)
+      , number_of_skinned_bones(geo.number_of_skinned_bones)
+      , start_bones(geo.start_bones)
+      , root_bone(geo.root_bone)
+      , number_of_bones(geo.number_of_bones)
+      , radius(geo.radius)
+      , display(false)
+      , bounding_box()
+    {
+      bounding_box[0] = geo.bounding_box[0];
+      bounding_box[1] = geo.bounding_box[1];
+    }
+
+    M2ModelGeosetHD(const M2ModelGeosetHD& geo)
+      : id(geo.id)
+      , vstart(geo.vstart)
+      , vcount(geo.vcount)
+      , istart(geo.istart)
+      , icount(geo.icount)
+      , number_of_skinned_bones(geo.number_of_skinned_bones)
+      , start_bones(geo.start_bones)
+      , root_bone(geo.root_bone)
+      , number_of_bones(geo.number_of_bones)
+      , radius(geo.radius)
+      , display(geo.display)
+      , bounding_box()
+    {
+      bounding_box[0] = geo.bounding_box[0];
+      bounding_box[1] = geo.bounding_box[1];
+    }
+
+    u32 id;                      // mesh part id?
+    u16 vstart;                  // first vertex, Starting vertex number.
+    u16 vcount;                  // num vertices, Number of vertices.
+    u32 istart;                  // first index, Starting triangle index (that's 3* the number of triangles drawn so far).
+    u16 icount;                  // num indices, Number of triangle indices.
+    u16 number_of_skinned_bones; // number of bone indices, Number of elements in the bone lookup table.
+    u16 start_bones;             // ? always 1 to 4, Starting index in the bone lookup table.
+    u16 root_bone;               // root bone?
+    u16 number_of_bones;         //
+    glm::vec3 bounding_box[2];
+    float radius;
+    bool display;
+  };
+
+#pragma pack(pop)
+
   class M2ModelView : public AssetWrapper<M2ModelView>
   {
+    friend class M2Model;
+
   protected:
     void on_fully_loaded() override;
 
@@ -41,7 +134,7 @@ namespace loki {
       u32 offset;
     };
 
-    struct Data
+    struct Header
     {
       char id[4]; // Signature
       Record index;
@@ -54,8 +147,9 @@ namespace loki {
 
 #pragma pack(pop)
 
-    Data* data{};
+    Header* header = nullptr;
     std::vector<u32> raw_indices;
+    std::vector<M2ModelGeosetHD> raw_geosets;
   };
 
 } // namespace loki
