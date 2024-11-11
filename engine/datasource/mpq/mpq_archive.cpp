6197c657a8a1a06ce97715b93a8c29dc55c64ce4
Vladislav Belousov <vladbelousov@yahoo.com>
2024-11-11 12:39:27 +0000

    Make loading faster by using MPQ_OPEN_NO_LISTFILE

--- engine/datasource/mpq/mpq_archive.cpp
+++ engine/datasource/mpq/mpq_archive.cpp
@@ -22,7 +22,7 @@
 
 loki::MPQArchive::MPQArchive(const std::filesystem::path& path)
 {
-  if (!SFileOpenArchive(path.string().c_str(), 0, 0x00000100 /* Read-only */, &handle)) {
+  if (!SFileOpenArchive(path.string().c_str(), 0, MPQ_OPEN_NO_LISTFILE | MPQ_OPEN_READ_ONLY, &handle)) {
     spdlog::error("Error opening MPQ archive: {}", path.string());
   }
 }
